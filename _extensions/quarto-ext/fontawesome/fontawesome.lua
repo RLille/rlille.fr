401362b3ad5c6c07f0ef47c28e67a5b6e788883c
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-07-10 21:20:14 +0200

    fix: fa icons and update quarto

--- _extensions/quarto-ext/fontawesome/fontawesome.lua
+++ _extensions/quarto-ext/fontawesome/fontawesome.lua
@@ -6,24 +6,24 @@ function ensureHtmlDeps()
   quarto.doc.addHtmlDependency({
     name = 'fontawesome6',
     version = '0.1.0',
-    stylesheets = {
-      {
-        name = 'assets/css/all.css',
-        path = 'assets/css/all.css'
-      }
-    }
+    stylesheets = {'assets/css/all.css'}
   })
 end
 
 return {
   ["fa"] = function(args, kwargs)
 
+    local group = "solid"
     local icon = pandoc.utils.stringify(args[1])
+    if #args > 1 then
+      group = icon
+      icon = pandoc.utils.stringify(args[2])
+    end
 
     -- detect html (excluding epub which won't handle fa)
     if quarto.doc.isFormat("html:js") then
       ensureHtmlDeps()
-      return pandoc.RawInline('html', "<i class=\"fa-solid fa-" .. icon .. "\"></i>")
+      return pandoc.RawInline('html', "<i class=\"fa-" .. group .. " fa-" .. icon .. "\"></i>")
     -- detect pdf / beamer / latex / etc
     elseif quarto.doc.isFormat("pdf") then
       ensureLatexDeps()
